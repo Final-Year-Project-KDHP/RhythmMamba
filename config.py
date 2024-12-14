e6847a9ab84d1a91389c66924f2f50794ce655ef
A. Kamesh Anuradha <98675082+askanuradha@users.noreply.github.com>
2024-12-14 14:47:36 +0530

    added_vid_idx_method

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ from yacs.config import CfgNode as CN
 
 _C = CN()
 
+_C.VID_IDX = 0
+
 # Base config files
 _C.BASE = ['']
 # -----------------------------------------------------------------------------
